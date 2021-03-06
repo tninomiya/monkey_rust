b49a9b6558ab66af93d62a910a964c8ef9e61462
Tomoki Ninomiya <t.miliya612@gmail.com>
2021-03-06 23:04:00 +0900

    utilze enum for token

--- token/src/token.rs
+++ token/src/token.rs
@@ -1,17 +1,104 @@
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Token {
     token_type: TokenKind,
-    literal: String,
 }
 
+impl Token {
+    pub fn illegal() -> Self {
+        Token {
+            token_type: TokenKind::Illegal,
+        }
+    }
+
+    pub fn eof() -> Self {
+        Token {
+            token_type: TokenKind::Eof,
+        }
+    }
+
+    pub fn ident(s: String) -> Self {
+        Token {
+            token_type: TokenKind::Ident(s),
+        }
+    }
+
+    pub fn int(n: i64) -> Self {
+        Token {
+            token_type: TokenKind::Int(n),
+        }
+    }
+
+    pub fn assign() -> Self {
+        Token {
+            token_type: TokenKind::Assign,
+        }
+    }
+
+    pub fn plus() -> Self {
+        Token {
+            token_type: TokenKind::Plus,
+        }
+    }
+
+    pub fn comma() -> Self {
+        Token {
+            token_type: TokenKind::Comma,
+        }
+    }
+
+    pub fn semicolon() -> Self {
+        Token {
+            token_type: TokenKind::Semicolon,
+        }
+    }
+
+    pub fn lparen() -> Self {
+        Token {
+            token_type: TokenKind::LParen,
+        }
+    }
+
+    pub fn rparen() -> Self {
+        Token {
+            token_type: TokenKind::RParen,
+        }
+    }
+
+    pub fn lbrace() -> Self {
+        Token {
+            token_type: TokenKind::LBrace,
+        }
+    }
+
+    pub fn rbarce() -> Self {
+        Token {
+            token_type: TokenKind::RBrace,
+        }
+    }
+
+    pub fn keyword_function() -> Self {
+        Token {
+            token_type: TokenKind::Function,
+        }
+    }
+
+    pub fn keyword_let() -> Self {
+        Token {
+            token_type: TokenKind::Let,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum TokenKind {
     Illegal,
     Eof,
 
     // identifier + literal
     // add, foobar, x, y,...
-    Ident,
+    Ident(String),
     // 123456
-    Int,
+    Int(i64),
 
     // operator
     // "="
